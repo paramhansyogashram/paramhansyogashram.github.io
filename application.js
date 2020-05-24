c07555e6053f2d171ac964620bab45f926354aae
Ramratan Gupta <ramratan.gupta@gmail.com>
2020-05-24 18:23:22 +0530

    header issue

--- application.js
+++ application.js
@@ -1,7 +1,16 @@
 jQuery( document ).ready(function() {
+	jQuery('#header').load('header.html',function(){
+		jQuery("#header li").removeClass("active");
+		var currElementHfef = jQuery('#header li a').filter(function(){
+  			return this.href === location.href;
+		});
+		if(currElementHfef.length == 1){
+			jQuery(currElementHfef[0]).parent().addClass("active");
+		}
+	});
 	jQuery('#footer').load('footer.html',function(){
 		var d = new Date();
-		jQuery('#copyright').html("C"+d.getFullYear());
+		jQuery('#copyright').html(d.getFullYear());
 	});
 	
 });
