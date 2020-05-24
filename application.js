46fe078134047b222b54e9d965d6cef1a54f656d
Ramratan Gupta <ramratan.gupta@gmail.com>
2020-05-24 18:30:30 +0530

    header

--- application.js
+++ application.js
@@ -1,11 +1,13 @@
 jQuery( document ).ready(function() {
 	jQuery('#header').load('header.html',function(){
-		jQuery("#header li").removeClass("active");
-		var currElementHfef = jQuery('#header li a').filter(function(){
-  			return this.href === location.href;
-		});
-		if(currElementHfef.length == 1){
-			jQuery(currElementHfef[0]).parent().addClass("active");
+		if(document.location.href!="https://www.paramhansyogashram.com/"){
+			jQuery("#header li").removeClass("active");
+			var currElementHfef = jQuery('#header li a').filter(function(){
+	  			return this.href === location.href;
+			});
+			if(currElementHfef.length == 1){
+				jQuery(currElementHfef[0]).parent().addClass("active");
+			}
 		}
 	});
 	jQuery('#footer').load('footer.html',function(){
