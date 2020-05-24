93f91619956508026f15ef7270ea74cdff9a89a7
Ramratan Gupta <ramratan.gupta@gmail.com>
2020-05-24 17:55:52 +0530

    fix footer

--- application.js
+++ application.js
@@ -1,7 +1,9 @@
 jQuery( document ).ready(function() {
-	jQuery('#footer').load('footer.html');
-	var d = new Date();
-	jQuery('#copyright').html("C"+d.getFullYear());
+	jQuery('#footer').load('footer.html',{},function(){
+		var d = new Date();
+		jQuery('#copyright').html("C"+d.getFullYear());
+	});
+	
 });
 window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
