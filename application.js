c6ce06a7dcd2890b50e44a91cc89bbe138e6dada
Ramratan Gupta <ramratan.gupta@gmail.com>
2020-05-24 17:38:47 +0530

    footer

---
+++ application.js
@@ -0,0 +1,7 @@
+jQuery( document ).ready(function() {
+	jQuery('#footer').load('footer.html');
+});
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'UA-54701667-1');
