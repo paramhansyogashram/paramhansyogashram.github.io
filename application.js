fa3c6337aba371684ba06cf10c8a3c5ce498d816
Ramratan Gupta <ramratan.gupta@gmail.com>
2025-03-12 16:28:32 +0530

    remove old GA tag

--- application.js
+++ application.js
@@ -16,7 +16,3 @@ jQuery( document ).ready(function() {
 	});
 	
 });
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-gtag('config', 'UA-54701667-1');
